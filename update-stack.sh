18fc88e01cb48170a647a21fb4c3e161f4dec532
OmarElsheekh <omarelsheekh@manager.omar.com>
2021-01-18 11:02:32 -0500

    add a internet gateway

--- update-stack.sh
+++ update-stack.sh
@@ -1,6 +1,6 @@
 #   chmod +x update-stack.sh
 #   ./update-stack.sh mystack infra.yml infra-params.json
-aws cloudformation update-stack 
+aws cloudformation update-stack \
     --stack-name $1  \
     --template-body file://$2   \
     --parameters file://$3  \
