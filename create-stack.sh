0f395d892d3bcc4d1ff7c780c4b21c802c5d1644
OmarElsheekh <omarelsheekh@manager.omar.com>
2021-01-18 10:44:11 -0500

    add a vpc

--- create-stack.sh
+++ create-stack.sh
@@ -1,5 +1,7 @@
-aws cloudformation create-stack 
---stack-name $1 
---template-body file://$2  
---parameters file://$3 
---region=us-west-2
+#   chmod +x create-stack.sh
+#   ./create-stack.sh mystack infra.yml infra-params.json
+aws cloudformation create-stack \
+    --stack-name $1  \
+    --template-body file://$2   \
+    --parameters file://$3  \
+    --region=us-west-2
--- update-stack.sh
+++ update-stack.sh
@@ -1,5 +1,7 @@
+#   chmod +x update-stack.sh
+#   ./update-stack.sh mystack infra.yml infra-params.json
 aws cloudformation update-stack 
---stack-name $1 
---template-body file://$2  
---parameters file://$3 
---region=us-west-2
+    --stack-name $1  \
+    --template-body file://$2   \
+    --parameters file://$3  \
+    --region=us-west-2
